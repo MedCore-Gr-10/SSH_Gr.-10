5efa8f8f53d9f9da0c3c5d9b073a633463aa8ff3
Albison Bekaj <albison.bekaj@student.uni-pr.edu>
2026-04-25 19:33:59 +0200

    Ndryshime ne shtresen middlewear

--- backend/src/controllers/authController.js
+++ backend/src/controllers/authController.js
@@ -2,7 +2,7 @@
  *  {@link AuthService}.
  */
 export class AuthController {
-  /** @param {import("../services/auth.service.js").AuthService} authService */
+  /** @param {import("../services/authService.js").AuthService} authService */
   constructor(authService) {
     this.authService = authService;
   }
@@ -21,25 +21,6 @@ export class AuthController {
     try {
       const result = await this.authService.registerPatient(req.body);
       res.status(201).json(result);
-    } catch (err) {
-      res.status(400).json({ error: err.message });
-    }
-  };
-
-  selectHospitals = async (req, res) => {
-    try {
-      const user_id = req.user.user_id;
-      const { hospital_id } = req.body;
-
-      if (!hospital_id) {
-        return res.status(400).json({ error: "hospital_id is required" });
-      }
-
-      const result = await this.authService.selectHospital(
-        user_id,
-        hospital_id,
-      );
-      res.json(result);
     } catch (err) {
       res.status(400).json({ error: err.message });
     }
