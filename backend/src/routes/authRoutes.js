5af00a314862a3ea5d880887fae1f236f95036e6
Albison Bekaj <albison.bekaj@student.uni-pr.edu>
2026-04-25 19:34:41 +0200

    Ndryshim ne emeritim te file authRouter

--- backend/src/routes/authRoutes.js
+++ backend/src/routes/authRoutes.js
@@ -1,9 +1,9 @@
 import express from "express";
 import prisma from "../prisma.js";
 import { JwtService } from "../utils/jwt.js";
-import { AuthService } from "../services/auth.service.js";
-import { AuthController } from "../controllers/auth.controller.js";
-import { AuthMiddleware } from "../middlewares/auth.middleware.js";
+import { AuthService } from "../services/authService.js";
+import { AuthController } from "../controllers/authController.js";
+import { AuthMiddleware } from "../middlewares/authMiddleware.js";
 
 const router = express.Router();
 
@@ -14,10 +14,5 @@ const authMiddleware = new AuthMiddleware(jwtService);
 
 router.post("/register", authController.register);
 router.post("/login", authController.login);
-router.post(
-  "/select-hospital",
-  authMiddleware.handle,
-  authController.selectHospitals
-);
 
 export default router;
