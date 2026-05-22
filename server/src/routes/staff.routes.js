6e924944c6df558fd220d168feeee54cfdf3fced
olsadomi <olsadomi@gmail.com>
2026-05-22 22:32:07 +0200

    SCRUM-45 #comment completed  staff routes swagger definitons

--- server/src/routes/staff.routes.js
+++ server/src/routes/staff.routes.js
@@ -16,6 +16,24 @@ router.use((req, res, next) => authMiddleware.handle(req, res, next));
 router.use((req, res, next) => hospitalMiddleware.handle(req, res, next));
 router.use((req, res, next) => roleMiddleware.handle(req, res, next));
 
+/**
+ * @swagger
+ * /api/staff/schedules:
+ *   get:
+ *     summary: Get staff schedules
+ *     description: Returns schedules for authenticated staff members
+ *     tags:
+ *       - Staff
+ *     security:
+ *       - bearerAuth: []
+ *     responses:
+ *       200:
+ *         description: Staff schedules retrieved successfully
+ *       401:
+ *         description: Unauthorized
+ *       403:
+ *         description: Forbidden
+ */
 router.get("/schedules", (req, res, next) => staffScheduleController.getStaffSchedules(req, res, next));
 
 export default router;
